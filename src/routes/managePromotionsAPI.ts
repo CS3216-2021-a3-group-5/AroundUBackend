1e48bcdc42a8b8cbf1d826efca8a87d535c81114
YuFeng <leo20145kl@gmail.com>
2021-09-22 05:37:36 +0800

    Fix CORS, JSON.parse

--- src/routes/managePromotionsAPI.ts
+++ src/routes/managePromotionsAPI.ts
@@ -5,13 +5,14 @@ import { BADREQUEST, OK } from "../statuscodes/statusCode"
 
 export async function createNewPromotion(req: Request, res: Response) {
     try {
+        const body = JSON.parse(req.body)
         await saveNewPromotion({
-            promotion_id: req.body.promotion_id,
+            promotion_id: body.promotion_id,
             company_name: res.locals.jwt.company_name,
-            promo_name: req.body.promo_name,
-            end_date: req.body.end_date,
-            details: req.body.details,
-            storeIDs: req.body.store_ids
+            promo_name: body.promo_name,
+            end_date: body.end_date,
+            details: body.details,
+            storeIDs: body.store_ids
         })
         return res.status(OK).send("Creation success!")
     } catch (err) {
@@ -24,7 +25,7 @@ export async function getUserPromotions(req: Request, res: Response) {
     try {
         const promos = await getListOfPromotionsOfCompany(res.locals.jwt.company_name)
         res.status(200).json({
-            "promotions": promos
+            promotions: promos
         })
     } catch (err) {
         res.status(BADREQUEST).send(err)
--- src/routes/manageStoresAPI.ts
+++ src/routes/manageStoresAPI.ts
@@ -3,17 +3,18 @@ import { saveNewStore } from "../models/store"
 import { getListOfStoresOfCompany } from "../models/store"
 import { BADREQUEST, FORBIDDEN, OK } from "../statuscodes/statusCode"
 export async function createNewStore(req: Request, res: Response) {
+    const body = JSON.parse(req.body)  
     try {
         await saveNewStore({
-            store_id: req.body.store_id,
+            store_id: body.store_id,
             company_name: res.locals.jwt.company_name,
             location: {
-                lon: req.body.longitude,
-                lat: req.body.latitude,
+                lon: body.longitude,
+                lat: body.latitude,
             },
-            address: req.body.address,
-            opening_hours: req.body.opening_hours,
-            promotionIDs: req.body.promotion_ids
+            address: body.address,
+            opening_hours: body.opening_hours,
+            promotionIDs: body.promotion_ids
         })
         return res.status(OK).send("Creation success!")
     } catch (err) {
@@ -22,11 +23,13 @@ export async function createNewStore(req: Request, res: Response) {
 }
 
 export async function getUserStore(req: Request, res: Response) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     try {
       let stores = await getListOfStoresOfCompany(res.locals.jwt.company_name)
         
       return res.status(OK).json({
-          "stores": stores
+          stores: stores
       })
     } catch (err) {
       return res.status(FORBIDDEN).send(err)
